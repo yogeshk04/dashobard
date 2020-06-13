a89d758d87b0471f535f13f96845aba7e73f9e34
yogeshk04 <yogeshk04@gmail.com>
2020-06-13 14:40:49 +0530

    Add country end point

--- src/main/java/com/base/dashboad/controllers/CountryController.java
+++ src/main/java/com/base/dashboad/controllers/CountryController.java
@@ -1,14 +1,37 @@
 package com.base.dashboad.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.base.dashboad.models.Country;
+import com.base.dashboad.services.CountryService;
 
 @Controller
 public class CountryController {
 	
+	@Autowired
+	private CountryService countryService;
+	
 	@GetMapping("/countries")
-	public String getCountries() {
+	public String getCountries(Model model) {
+		
+		List<Country> countryList = countryService.getCountries();
+		model.addAttribute("countries", countryList);
+
 		return "countries";
+	}
+	
+	@PostMapping("/countries/addNewCountry")
+	public String addNewCountry(Country country) {
+		countryService.save(country);
+		
+		return "redirect:/countries";
 	}
 
 }
+	
--- src/main/java/com/base/dashboad/services/CountryService.java
+++ src/main/java/com/base/dashboad/services/CountryService.java
@@ -1,8 +1,26 @@
 package com.base.dashboad.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.base.dashboad.models.Country;
+import com.base.dashboad.repositories.CountryRepository;
 
 @Service
 public class CountryService {
-
+	
+	@Autowired
+	private CountryRepository countryRepository;
+	
+	
+	public List<Country> getCountries(){
+		return countryRepository.findAll();
+	}
+	
+	// add new country
+	public void save(Country country) {
+		countryRepository.save(country);
+	}
 }
